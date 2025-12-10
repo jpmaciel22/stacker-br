5762fd6659020e325685b6aa3faf4fe874087ab4
jpmaciel22 <jpbmaciel@gmail.com>
2025-12-10 10:15:00 -0300

    first finished language project

--- stacker.rkt
+++ stacker.rkt
@@ -10,5 +10,25 @@
 (provide read-syntax)
 (define-macro (stacker-module-begin HANDLE-EXPR ...)
   #'(#%module-begin
-     'HANDLE-EXPR ...))
+     HANDLE-EXPR ...
+     (display (first stack))))
 (provide (rename-out [stacker-module-begin #%module-begin]))
+
+(define stack empty)
+
+(define (pop-stack!)
+  (define arg (first stack))
+  (set! stack (rest stack))
+  arg)
+
+(define (push-stack! arg)
+  (set! stack (cons arg stack)))
+
+(define (handle [arg #f]) ;; that way we can make the arg optional
+  (cond
+    [(number? arg) (push-stack! arg)] ;; if its a number push the arg
+    [(or (equal? + arg) (equal? * arg))
+     (define op-result (arg (pop-stack!) (pop-stack!))) ;; se for alguma operacao, nos damos 2 pop pra pegar os valores e aplicamos em arg com uma expression call
+     (push-stack! op-result)]))
+(provide  handle)
+(provide + *) ;; tem q dar provide nessas funcoes tb mas o quicklang ja tem entao nos pegamos emprestado dele
